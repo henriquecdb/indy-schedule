ad5ae0e318693fec1fb777ccf803d302a33fb72e
Henrique Junqueira <henriquecamposj@gmail.com>
2026-03-02 07:53:35 -0300

    Initial Commit

---
+++ indy-schedule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  indy-schedule
+//
+//  Created by Henrique Junqueira on 02/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ indy-schedule/indy_scheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  indy_scheduleApp.swift
+//  indy-schedule
+//
+//  Created by Henrique Junqueira on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct indy_scheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
