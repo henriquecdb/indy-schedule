7d57d6512c688162e1d846c7e9e8e149d8966965
Henrique Junqueira <henriquecamposj@gmail.com>
2026-03-09 20:08:52 -0300

    chore: add new badge to indicate completed races

--- indy-schedule/ContentView.swift
+++ indy-schedule/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
 
     var body: some View {
         let nextRace = RaceStore.nextRace(from: races)
+        let startOfToday = Calendar.current.startOfDay(for: .now)
 
         NavigationStack {
             List {
@@ -48,6 +49,13 @@ struct ContentView: View {
                                     .padding(.vertical, 6)
                                     .background(.red.opacity(0.14), in: Capsule())
                                     .foregroundStyle(.red)
+                            } else if race.date < startOfToday {
+                                Text("Encerrada")
+                                    .font(.caption.weight(.bold))
+                                    .padding(.horizontal, 10)
+                                    .padding(.vertical, 6)
+                                    .background(.gray.opacity(0.18), in: Capsule())
+                                    .foregroundStyle(.secondary)
                             }
                         }
                         .padding(.vertical, 6)
